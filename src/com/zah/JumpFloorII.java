10ef1e3f12b5e70e799ea64aabc778e63e5a23ba
zhanganhua <zhanganhua@corp.neteasy.com>
2019-07-30 14:10:26 +0800

    测试

--- src/com/zah/JumpFloorII.java
+++ src/com/zah/JumpFloorII.java
@@ -4,6 +4,7 @@ public class JumpFloorII {
 
     int jumpFloorII(int number) {
 
+        System.out.println();
         if (number == 0)return 0;
         if (number==1) return 1;
         return 2*jumpFloorII(number-1);
