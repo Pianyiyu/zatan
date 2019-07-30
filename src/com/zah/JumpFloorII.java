24a64a4aa54c280704dcd9719cedc3f5ed2db863
zhanganhua <zhanganhua@corp.neteasy.com>
2019-07-30 15:09:58 +0800

    final

--- src/com/zah/JumpFloorII.java
+++ src/com/zah/JumpFloorII.java
@@ -5,7 +5,6 @@ import java.util.AbstractCollection;
 public class JumpFloorII {
 
     int jumpFloorII(int number) {
-        if (number == 0)return 0;
 
         if (number == 0)return 0;
         if (number==1) return 1;
