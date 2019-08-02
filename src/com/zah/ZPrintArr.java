59e37c4e65d3751582830bbab17efe23d1c5c270
可怜巴巴 <34368398+Pianyiyu@users.noreply.github.com>
2019-08-02 16:30:16 +0800

    test

--- src/com/zah/ZPrintArr.java
+++ src/com/zah/ZPrintArr.java
@@ -3,7 +3,7 @@ package com.zah;
 public class ZPrintArr {
 
     public static void main(String[] args) {
-        //远程新增
+        //远程新增new
         int[][] arr = {{1, 2, 3, 4}, {5, 6, 7, 8},{9,10,11,12}};
         zPrintArr(arr);
         //本地新增
